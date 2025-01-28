e2d37cb13ba6a4ee07f0f1d5f99ad233a24b1b38
AdrianBravo-doto <abravo@doto.com.mx>
2025-01-28 12:51:19 -0600

    Nuevo almacen

--- webhook_stock/models/webhook_stock.py
+++ webhook_stock/models/webhook_stock.py
@@ -20,7 +20,7 @@ class StockWebhook(models.Model):
 
     def _trigger_webhook(self, record):
         
-        if record.location_id.id not in [28, 1867]:
+        if record.location_id.id not in [28, 1867, 1869]:
             return
         webhook_url = "https://odoo.doto.com.mx/api/v2/odoo/hook/products/sync"
         payload = {
