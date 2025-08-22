093533dee4d9aad4696cb3f1972fa7a36e837cb8
ErikLSantiago <108760107+ErikLSantiago@users.noreply.github.com>
2025-08-22 16:35:47 -0600

    Add files via upload

--- website_invitation_visibility/models/website_page.py
+++ website_invitation_visibility/models/website_page.py
@@ -15,9 +15,15 @@ class IrUiView(models.Model):
         res = super()._is_visible(website=website, **kwargs)
         for view in self:
             if getattr(view, 'visibility', None) == 'invitation':
-                user = view.env.user
+                user = view.env.user.sudo()
                 if user._is_public():
                     res = False
                 else:
-                    res = bool(user.partner_id.sudo().is_wholesale_access)
+                    p = user.partner_id.sudo()
+                    allowed = bool(p and (
+                        getattr(p, 'is_wholesale_access', False) or
+                        (p.parent_id and getattr(p.parent_id, 'is_wholesale_access', False)) or
+                        (p.commercial_partner_id and getattr(p.commercial_partner_id, 'is_wholesale_access', False))
+                    ))
+                    res = allowed
         return res
