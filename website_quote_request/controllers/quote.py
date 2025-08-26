ff2b45d25a95726ed602e969af728c6e3f97cf25
ErikLSantiago <108760107+ErikLSantiago@users.noreply.github.com>
2025-08-26 13:28:36 -0600

    Add files via upload

---
+++ website_quote_request/controllers/quote.py
@@ -0,0 +1,32 @@
+
+from odoo import http, _
+from odoo.http import request
+from odoo.tools import html_sanitize
+
+class WebsiteQuoteRequest(http.Controller):
+
+    @http.route(['/quote'], type='http', auth='public', website=True, sitemap=True)
+    def quote_form(self, **kwargs):
+        values = {}
+        return request.render('website_quote_request.quote_form', values)
+
+    @http.route(['/quote/submit'], type='http', auth='public', website=True, csrf=True)
+    def quote_submit(self, **post):
+        comments = (post.get('comments') or '').strip()
+
+        partner = request.env.user.partner_id.sudo() if request.env.user and not request.env.user._is_public() else request.env['res.partner'].sudo().create({'name': _('Cliente Website')})
+        order = request.env['sale.order'].sudo().create({'partner_id': partner.id})
+
+        safe_comments = html_sanitize(comments) if comments else ''
+
+        body = _("Solicitud de cotización enviada desde el sitio web.")
+        if safe_comments:
+            body += "<br/><br/><b>%s</b><br/>%s" % (_('Comentarios del cliente:'), safe_comments)
+
+        order.message_post(body=body)
+
+        return request.redirect('/quote/thanks')
+
+    @http.route(['/quote/thanks'], type='http', auth='public', website=True, sitemap=False)
+    def quote_thanks(self, **kwargs):
+        return request.render('website_quote_request.quote_thanks', {})
