17bba0e4a94cf5fdd21662e9faa347f9f81bacf8
ErikLSantiago <108760107+ErikLSantiago@users.noreply.github.com>
2025-09-05 17:32:42 -0600

    Add files via upload

---
+++ adicional_serial_number/__manifest__.py
@@ -0,0 +1,19 @@
+
+{
+    "name": "Adicional Serial Number",
+    "summary": "Captura adicional de números de serie por picking/producto sin tocar la trazabilidad nativa.",
+    "version": "18.0.1.30.1",
+    "author": "Tu Equipo",
+    "category": "Inventory/Logistics",
+    "license": "LGPL-3",
+    "depends": ["stock"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/version.xml",
+        "actions/serial_actions.xml",
+        "views/serial_views.xml",
+        "views/stock_picking_views_inherit.xml"
+    ],
+    "installable": True,
+    "application": False
+}
