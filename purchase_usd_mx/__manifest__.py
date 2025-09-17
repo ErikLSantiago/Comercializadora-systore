7d4669d485534488107e6af6ad32d5ba324d27b3
ErikLSantiago <108760107+ErikLSantiago@users.noreply.github.com>
2025-09-17 15:51:58 -0600

    Add files via upload

---
+++ purchase_usd_mx/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Purchase USD MX",
+    "summary": "Pestaña USD en compras con cálculo Precio USD y Total USD.",
+    "version": "18.0.1.0.2",
+    "category": "Purchases",
+    "author": "Systore + ChatGPT",
+    "license": "LGPL-3",
+    "depends": ["purchase"],
+    "data": [
+        "views/purchase_usd_views.xml"
+    ],
+    "installable": True,
+    "application": False
+}
