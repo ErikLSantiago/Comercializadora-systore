9882ecc5962b43deb9449b6de18b74c99b4bcd77
ErikLSantiago <108760107+ErikLSantiago@users.noreply.github.com>
2025-11-25 23:35:25 -0600

    Add files via upload

---
+++ systore_warranty_sale_purchase_bridge/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Systore Warranty Sale-Purchase Bridge",
+    "summary": "Vincula tickets de garantía con pedidos de venta y genera órdenes de compra en Scrap Systore.",
+    "version": "18.0.1.0.4",
+    "author": "Comercializadora Systore",
+    "website": "https://systore.com.mx",
+    "license": "LGPL-3",
+    "category": "Sales",
+    "depends": [
+        "sale_management",
+        "purchase",
+        "helpdesk",
+        "systore_warranty_helpdesk"
+    ],
+    "data": [
+        "views/sale_order_views.xml"
+    ],
+    "installable": True,
+    "application": False
+}
