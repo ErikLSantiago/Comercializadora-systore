1ac7eac2e64f07df31e3857b78b1788f045f4e79
ErikLSantiago <108760107+ErikLSantiago@users.noreply.github.com>
2025-12-12 20:08:18 -0600

    Add files via upload

--- adicional_serial_number/models/stock_move_line_serial.py
+++ adicional_serial_number/models/stock_move_line_serial.py
@@ -33,6 +33,7 @@ class StockMoveLineSerial(models.Model):
                 val = False
             rec.carrier_tracking_ref = val
 
+    # Bloquear edición si picking validado/cancelado
     def _check_editable_state(self):
         for rec in self:
             if rec.picking_id and rec.picking_id.state in ('done', 'cancel'):
