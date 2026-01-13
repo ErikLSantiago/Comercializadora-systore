c96531fe751b859edc3ffcf8bb3acb0718b67f2f
ErikLSantiago <108760107+ErikLSantiago@users.noreply.github.com>
2026-01-12 18:31:56 -0600

    Add files via upload

--- partner_income_account/__manifest__.py
+++ partner_income_account/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Partner Income Account on Invoices",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "Accounting",
     "summary": "Assign income account on invoice lines based on customer (partner).",
     "license": "LGPL-3",
