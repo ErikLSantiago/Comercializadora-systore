40d8a15a381d100dbb95015810dd1fe12eee4ccc
ErikLSantiago <108760107+ErikLSantiago@users.noreply.github.com>
2026-01-14 17:32:45 -0600

    Add files via upload

--- marketplace_settlement_reconcile/__manifest__.py
+++ marketplace_settlement_reconcile/__manifest__.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Marketplace Settlement Reconcile (CSV)",
-    "version": "18.0.1.0.8",
+    "version": "18.0.1.0.10",
     "category": "Accounting",
     "summary": "Mass reconcile marketplace payouts against invoices using CSV settlement lines",
     "author": "Systore",
     "license": "LGPL-3",
-    "depends": ["account", "sale_management"],
+    "depends": ["account", "account_accountant", "sale_management"],
     "data": [
         "security/ir.model.access.csv",
         "views/res_config_settings_views.xml",
         "wizard/import_settlement_csv_views.xml",
         "views/marketplace_settlement_views.xml",
+        "views/bank_rec_widget_quick_create_views.xml",
+        "views/bank_rec_widget_kanban_views.xml",
     ],
     "application": False,
     "installable": True,
