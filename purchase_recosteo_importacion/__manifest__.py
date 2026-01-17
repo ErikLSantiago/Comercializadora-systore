2684cb653bd5eb3ec71928a5795f496fcae45644
ErikLSantiago <108760107+ErikLSantiago@users.noreply.github.com>
2026-01-17 00:57:31 -0600

    Add files via upload

--- purchase_recosteo_importacion/__manifest__.py
+++ purchase_recosteo_importacion/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Recosteo de compras (Importación fija + TC)',
-    'version': '18.0.1.0.5',
+    'version': '18.0.1.0.7',
     'category': 'Purchases',
     'summary': 'Recosteo unitario con importación fija por producto (MXN) y tipo de cambio manual.',
     'description': """Recosteo de compras con tipo de cambio manual, importación fija por producto (MXN) y botón para aplicar al price_unit.""",
