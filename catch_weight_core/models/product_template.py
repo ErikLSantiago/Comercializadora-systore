b34b431c9bf156869daebb72588b3d7030de74c6
ErikLSantiago <108760107+ErikLSantiago@users.noreply.github.com>
2026-01-24 18:12:44 -0600

    Delete catch_weight_core directory

--- catch_weight_core/models/product_template.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    x_is_catch_weight = fields.Boolean("Catch Weight (peso variable)", default=False)
-    x_shelf_life_days = fields.Integer("Vida útil (días)", default=0, help="Se usa para calcular caducidad del lote desde la fecha de producción.")
