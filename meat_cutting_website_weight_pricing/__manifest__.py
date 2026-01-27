f14e79d96a3054e94c34bf97d86f69f4b9c40ced
ErikLSantiago <108760107+ErikLSantiago@users.noreply.github.com>
2026-01-26 19:30:44 -0600

    Add files via upload

--- meat_cutting_website_weight_pricing/__manifest__.py
+++ meat_cutting_website_weight_pricing/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Meat Cutting - Website Weight Pricing (Checkout Reservation)",
-    "version": "1.0.2",
+    "version": "1.0.0",
     "category": "Sales/Website",
     "summary": "Show price per weight on website and reserve lots at checkout to compute exact price.",
     "depends": ["website_sale", "sale_stock", "stock", "meat_cutting", "meat_cutting_weight_sale_price"],
     "data": ["security/ir.model.access.csv", "data/cron.xml", "views/product_views.xml", "views/website_sale_templates.xml"],
-    "assets": {"web.assets_frontend": ["meat_cutting_website_weight_pricing/static/src/js/mc_web_move_lot_summary.js"]},
+    "assets": {"web.assets_frontend": []},
     "installable": True,
     "application": False,
     "license": "LGPL-3",
